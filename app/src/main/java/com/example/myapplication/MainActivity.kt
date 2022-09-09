12693244a3e94829b9f494a15f63df44c416f5d9
zhengxiaoning <zhengxiaoning@didiglobal.com>
2022-09-09 22:09:18 +0800

    master first

---
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -0,0 +1,38 @@
+package com.example.myapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        findViewById<TextView>(R.id.text).setOnClickListener {
+           val dialogFragment = MyDialogFragment()
+            dialogFragment.show(supportFragmentManager,"aa")
+        }
+
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.d("zhengxiaoning","onStart")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d("zhengxiaoning","onResume")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d("zhengxiaoning","onPause")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d("zhengxiaoning","onStop")
+    }
+}
---
+++ app/src/main/java/com/example/myapplication/MyDialogFragment.kt
@@ -0,0 +1,22 @@
+package com.example.myapplication
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.DialogFragment
+import androidx.fragment.app.FragmentManager
+
+class MyDialogFragment: DialogFragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        return inflater.inflate(R.layout.my_dialog_fragment_main,container);
+    }
+    override fun show(manager: FragmentManager, tag: String?) {
+        super.show(manager, tag)
+    }
+}
